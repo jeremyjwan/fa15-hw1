877abfa2b22db6c64e359601500e253ff7de052f
jeremyjwan <jeremyjwan@berkeley.edu>
2015-09-18 10:22:45 -0700

    done hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,8 @@
 class Foobar
   # Q4 CODE HERE
+  
+  def self.baz(a)
+  	return a.map { |val| val.to_i }.map { |val| val += 2 if val % 2 == 0}.compact.uniq.select {|val| val < 11}.reduce(:+)
+  end
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,18 @@
 def squared_sum(a, b)
+	return (a+b)*(a+b)
   # Q1 CODE HERE
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a = a.sort
+  a2 = a.map{|item| item + 1}
+  return a2
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
